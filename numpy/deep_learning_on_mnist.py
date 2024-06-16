b78e8c8f1feff8245fb5ca011510d448c984c019
wangrl2016 <wangrl2016@gmail.com>
2024-06-16 23:13:48 +0800

    Load the MNIST data

--- numpy/deep_learning_on_mnist.py
+++ numpy/deep_learning_on_mnist.py
@@ -1,3 +1,5 @@
+import gzip
+import numpy
 import os
 import requests
 
@@ -24,3 +26,30 @@ if __name__ == '__main__':
             with open(file_path, 'wb') as fp:
                 for chunk in resp.iter_content(chunk_size=128):
                     fp.write(chunk)
+
+    mnist_dataset = {}
+
+    # images
+    for key in ('training_images', 'test_images'):
+        with gzip.open(os.path.join(data_dir, data_sources[key]), 'rb') as mnist_file:
+            mnist_dataset[key] = numpy.frombuffer(
+                mnist_file.read(), numpy.uint8, offset=16
+            ).reshape(-1, 28 * 28)
+
+    # labels
+    for key in ('training_labels', 'test_labels'):
+        with gzip.open(os.path.join(data_dir, data_sources[key]), 'rb') as mnist_file:
+            mnist_dataset[key] = numpy.frombuffer(mnist_file.read(), numpy.uint8, offset=8)
+
+    x_train, y_train, x_test, y_test = (
+        mnist_dataset['training_images'],
+        mnist_dataset['training_labels'],
+        mnist_dataset['test_images'],
+        mnist_dataset['test_labels'],
+    )
+
+    print('The shape of training images: {} and training labels: {}'.format(
+            x_train.shape, y_train.shape))
+    
+    print('The shape of test images: {} and test labels: {}'.format(
+        x_test.shape, y_test.shape))
