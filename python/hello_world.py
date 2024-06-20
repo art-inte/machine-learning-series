723ce7980ac84c60f9d7af0b67b4f29c641d7b1f
wangrl2016 <wangrl2016@gmail.com>
2024-06-20 03:30:26 +0000

    Common built-in functions

--- python/hello_world.py
+++ python/hello_world.py
@@ -1,2 +1,22 @@
 if __name__ == '__main__':
     print('Hello, world!')
+    # sep argument
+    print('Hello', 'world', sep='-')
+    # end argument
+    print('Hello', 'world', end=' ')
+
+    # common built-in functions
+    print(abs(-5))
+    print(round(3.14159, 2))
+    print(max(1, 3, 2))
+    print(sum([1, 2, 3])) 
+    print(int('42')) 
+    print(float('3.14'))
+    print(str(42))
+    print(list('hello'))
+    print(dict(a=1, b=2))
+    print(len('hello'))
+    print(sorted([3, 1, 2]))
+    print(list(reversed([1, 2, 3])))
+    print(type(42)) 
+    print(isinstance(42, int))
