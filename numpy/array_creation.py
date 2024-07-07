3e2e272b3e331dc051292367be406abdd07b829c
wangrl2016 <wangrl2016@gmail.com>
2024-07-07 05:10:44 +0000

    Array creation

--- numpy/array_creation.py
+++ numpy/array_creation.py
@@ -4,26 +4,46 @@ import matplotlib.pyplot as pyplot
 import numpy
 
 if __name__ == '__main__':
-    # 1. Array creation
     # Converting Python sequences to NumPy arrays.
     a1d = numpy.array([1, 2, 3, 4])
+    print('1D array:', a1d)
     a2d = numpy.array([[1, 2], [3, 4]])
+    print('2D array:', numpy.array_str(a2d).replace('\n', ''))
     a3d = numpy.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]], dtype=int)
+    print('3D array:', numpy.array_str(a3d).replace('\n', ''))
 
-    # 1D array creation functions
-    print(numpy.arange(2, 15, 2))
-    print(numpy.linspace(2, 15, 3))
+    print('Numpy default type:', a1d.dtype)
+
+    print(numpy.arange(10))
+    print(numpy.arange(2, 10, dtype=float))
+    print(numpy.arange(2, 3, 0.1))
+
+    print(numpy.linspace(0, 5, 5))
+    print(numpy.linspace(0, 1, 10, endpoint=False))
+    # draw sin function
+    x = numpy.linspace(0, 2 * numpy.pi, 200)
+    y = numpy.sin(x)
+    pyplot.plot(x, y)
+    pyplot.subplots_adjust(left=0.1, right=0.9, top=0.92, bottom=0.06)
+    pyplot.title('Plot of sin(x)')
+    pyplot.savefig('temp/draw_sin_with_linspace.png', dpi=300)
+    pyplot.show()
 
     # 2D array creation functions
     print(numpy.eye(3, 5))
     print(numpy.diag([1, 2, 3]))
     print(numpy.diag(numpy.array([[1, 2, 3], [4, 5, 6]])))
+    
+    print(numpy.vander((2, 4, 7, 9), 4))
 
     # general ndarray creation functions
     print(numpy.zeros((2, 3)))
     print(numpy.ones((2, 4)))
 
-    print(numpy.random.default_rng(42).random((2, 3)))
+    default_rng = numpy.random.default_rng(42)
+    print(default_rng.random((2, 3)))
+    # retain 3 decimal places
+    print(numpy.round(default_rng.random((2, 5)), 3))
 
     numpy.random.seed(42)
     mean1 = [1, 2]
